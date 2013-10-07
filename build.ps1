9251ad7547922d420dc61c7abb0c5547981a4140
Peter Sunde <peter.sunde@gmail.com>
2013-10-07 10:11:43 +0200

    fix broken myget build

--- build.ps1
+++ build.ps1
@@ -1,9 +1,5 @@
 ## THIS FILE IS USING TO BUILD A MYGET CI PACKAGE OF THIS REPOSITORY
 
-param(
-    [string]$packageVersion = $null
-)
-
 # Initialization
 $rootFolder = Split-Path -parent $script:MyInvocation.MyCommand.Path
 . $rootFolder\myget.include.ps1
@@ -11,6 +7,9 @@ $rootFolder = Split-Path -parent $script:MyInvocation.MyCommand.Path
 if(-not (MyGet-BuildRunner "myget")) {
     MyGet-Die "Try running .\examples\build.all.samples.ps1 DUDE!"
 }
+
+# Get package version
+$packageVersion = MyGet-Package-Version $packageVersion
 
 # Run powershell unit tests for myget.include.ps1
 git submodule update --init --recursive
