689c84f97e609f11135182eb28eedd4f443a0a2e
Peter Sunde <peter.sunde@gmail.com>
2013-10-07 12:53:12 +0200

    only reset environment variables if invalid buildrunner is detected

--- myget.ps1
+++ myget.ps1
@@ -9,16 +9,18 @@ param(
 $rootFolder = Split-Path -parent $script:MyInvocation.MyCommand.Path
 . $rootFolder\myget.include.ps1
 
+# Valid build runners
+$validBuildRunners = @("myget")
+
 # Fake build runner so that we can test that this script works.
 if($fakeBuildRunner) {
     MyGet-Set-EnvironmentVariable "BuildRunner" "myget"
     MyGet-Set-EnvironmentVariable "PackageVersion" "1.0.0"
-} else {
-    MyGet-Set-EnvironmentVariable "BuildRunner" ""
+} elseif(MyGet-BuildRunner -eq "") {
     MyGet-Set-EnvironmentVariable "PackageVersion" ""
 }
 
-if(-not (MyGet-BuildRunner "myget")) {
+if(-not ($validBuildRunners -contains (MyGet-BuildRunner))) {
     MyGet-Die "Try running .\examples\build.all.samples.ps1 DUDE!"
 }
 
