8f73395d96d3e8592faa6bf3a3c275698095bfaf
Peter Rekdal Sunde <peters@users.noreply.github.com>
2017-07-31 13:18:59 +0200

    Skip .0 notation for v4.7.

--- myget.include.ps1
+++ myget.include.ps1
@@ -938,7 +938,7 @@ function MyGet-Normalize-Paths {
 function MyGet-TargetFramework-To-Clr {
     param(
         [parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
-        [ValidateSet("v2.0", "v3.5", "v4.0", "v4.5", "v4.5.1", "v4.5.2", "v4.6", "v4.6.1", "v4.6.2", "v4.7.0")]
+        [ValidateSet("v2.0", "v3.5", "v4.0", "v4.5", "v4.5.1", "v4.5.2", "v4.6", "v4.6.1", "v4.6.2", "v4.7")]
         [string]$targetFramework
     )
 
@@ -972,7 +972,7 @@ function MyGet-TargetFramework-To-Clr {
 		"v4.6.2" {
 		    $clr = "net462"
 		}
-		"v4.7.0" {
+		"v4.7" {
 		    $clr = "net470"
 		}
     }
@@ -1018,7 +1018,7 @@ function MyGet-Clr-To-TargetFramework {
 			$targetFramework = "v4.6.2"
 		}
 		"net470" {
-			$targetFramework = "v4.7.0"
+			$targetFramework = "v4.7"
 		}
     }
 
@@ -1207,11 +1207,11 @@ function MyGet-Build-Project {
         [string]$version,
         
         [parameter(Position = 6, Mandatory = $false, ValueFromPipeline = $true)]
-        [ValidateSet("v1.1", "v2.0", "v3.5", "v4.0", "v4.5", "v4.5.1", "v4.5.2", "v4.6", "v4.6.1", "v4.6.2", "v4.7.0")]
+        [ValidateSet("v1.1", "v2.0", "v3.5", "v4.0", "v4.5", "v4.5.1", "v4.5.2", "v4.6", "v4.6.1", "v4.6.2", "v4.7")]
         [string[]]$targetFrameworks = @(),
 
         [parameter(Position = 7, Mandatory = $false, ValueFromPipeline = $true)]
-        [ValidateSet("v1.1", "v2.0", "v3.5", "v4.0", "v4.5", "v4.5.1", "v4.5.2", "v4.6", "v4.6.1", "v4.6.2", "v4.7.0")]
+        [ValidateSet("v1.1", "v2.0", "v3.5", "v4.0", "v4.5", "v4.5.1", "v4.5.2", "v4.6", "v4.6.1", "v4.6.2", "v4.7")]
         [string]$targetFramework = $null,
 
         [parameter(Position = 8, Mandatory = $true, ValueFromPipeline = $true)]
